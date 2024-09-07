1db29379b2978ce2684413b14dd323aba2c53956
Vaibhav <vaibhavanandav@gmail.com>
2024-09-08 02:26:32 +0530

    padding bottom for navigation

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -17,7 +17,6 @@ function Hero() {
 
   return (
     <section id='hero' className={styles.container}>
-        <div className='sectionTitle'></div>
             <div className={styles.colorModeContainer}>
                 <img className={styles.hero} src={heroImg} alt="Profile picture of Vaibhav Anand" onClick={toggleTheme}/>
             
